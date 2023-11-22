1eb47f5df5a81a19f2d7ee1a7bf9783cd894e10b
hansnery <hansnery@gmail.com>
2023-11-22 15:12:03 -0500

    Create post message view and route

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
+/* GET form for new message */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Post a New Message' }); // Assuming 'new.jade' is your form template
+});
+
+/* POST new message */
+router.post('/new', function(req, res, next) {
+  // Extract the message details from the request body
+  const user = req.body.user;
+  const text = req.body.text;
+  const added = new Date();
+
+  // Create a new message object
+  const newMessage = { text, user, added };
+
+  // Here, you would typically save the new message to a database
+  // For this example, we'll simulate saving by pushing to the 'messages' array
+  messages.push(newMessage);
+
+  // Redirect the user to the home page after posting a new message
+  res.redirect('/');
+});
+
+
 module.exports = router;
